ea6c7fe406c6e34972147c1489db0a112de18b72
Filip Jerga <jerga99@gmail.com>
2021-11-29 13:49:14 +0100

    Mut variables

--- main.rs
+++ main.rs
@@ -2,8 +2,16 @@
 
 
 fn main() {
-  let message = "Hello World";
+  let mut message = "Hello World";
+
+  println!("{}", message);
+  message = "Hi there";
   println!("Some text: {}", message);
+
+  let mut age = 10;
+  println!("{}", age);
+  age = 30;
+  println!("{}", age);
 }
 
 // binary executable code or library
