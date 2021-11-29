d8f91b02127298d7310f555a6f0efa3bd39579d0
Filip Jerga <jerga99@gmail.com>
2021-11-29 14:24:09 +0100

    Primitive types

--- main.rs
+++ main.rs
@@ -1,15 +1,22 @@
 
 
-
 fn main() {
-  let message = "Hello World";
-  let message_2 = print_welcome(message);
-  println!("{}", message_2);
-}
+  let is_it_fun = false;
+  // i32 -> signed integer of 32bits
+  // -> signed can hold positive and negative values
+  let num = -10;
 
-fn print_welcome(text: &str) -> &str {
-  println!("{}", text);
-  let new_message = "Hi There";
-  // return new_message;
-  new_message
+  // u32 -> 2^32 - 1
+  // u8 -> unsigned integer of 8bits
+  // 2^8 - 1 = 255
+  let small_num: u8 = 255;
+
+  // -2^7 -> 2^7 - 1
+  // -128 -> 127
+  let small_num_2: i8 = 127;
+
+  // operating system related type
+  let sys_num: isize = -10;
+  let sys_num_2: usize = 10;
+
 }
