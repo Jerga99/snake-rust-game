8e472ba5b4a53eb76b1e8545539c030d4dd12d0e
Filip Jerga <jerga99@gmail.com>
2021-11-30 12:22:59 +0100

    AI Stack

--- main.rs
+++ main.rs
@@ -1,20 +1,14 @@
 
 
 fn main() {
-    let float_num: f32 = 3.14;
-    let float_num_2: f64 = 3.2334327489;
+    let a = 10;
+    let b = a;
+    let c = 15;
+    let d = add(a, b);
+}
 
-    let tup: (i32, &str, u8) = (20, "Hello", 1);
 
-    println!("{}", tup.1);
-
-    let (a, b, c) = tup;
-    println!("{}", a);
-
-    let x = [1, 5, 6, 7];
-
-    println!("{}", x[2]);
-
-    let y = [2; 6]; // [2, 2, 2, 2, 2, 2]
-    println!("{}", y[5]);
+fn add(x: u32, y: u32) -> u32 {
+    let sum = x + y;
+    sum
 }
