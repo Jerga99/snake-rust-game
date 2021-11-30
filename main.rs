d6574c1f3962d5266ede102f153ef0d38429e95c
Filip Jerga <jerga99@gmail.com>
2021-11-30 14:04:43 +0100

    Copying age

--- main.rs
+++ main.rs
@@ -4,11 +4,19 @@ fn main() {
    let message = String::from("Hello");
    let message = extend_message(message);
 
+   let age = 30;
+   extend_age(age);
+   println!("{}", age);
+
    println!("{}", message);
 }
 
 fn extend_message(mut a: String) -> String {
     a.push_str(" World");
     a
+}
+
+fn extend_age(mut a: u32)  {
+    a += 100;
 }
 
