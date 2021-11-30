e8f4069a0c2f72233d1a4bf92e2b5f1754c3ea4b
Filip Jerga <jerga99@gmail.com>
2021-11-30 13:05:59 +0100

    String move

--- main.rs
+++ main.rs
@@ -3,12 +3,16 @@
 fn main() {
     // you can place on stack only values with static size
     let a = 10;
-    let b = a;
+    let b = a; // copying
     let c = 15;
     let d = add(a, b);
 
     let message = String::from("Hello");
-    println!("{}", message);
+    let message_2 = message;
+
+    //  cannot use message because it was moved to message_2
+    // println!("{}", message);
+    println!("{}", message_2);
 }
 
 
