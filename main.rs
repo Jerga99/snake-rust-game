c2ae00a4c8e5f0e0485dcd76a0a3d23dba36cf65
Filip Jerga <jerga99@gmail.com>
2021-11-30 13:27:08 +0100

    Moves additional explanation

--- main.rs
+++ main.rs
@@ -1,22 +1,12 @@
 
 
 fn main() {
-    // you can place on stack only values with static size
-    let a = 10;
-    let b = a; // copying
-    let c = 15;
-    let d = add(a, b);
-
-    let message = String::from("Hello");
-    let message_2 = message;
-
-    //  cannot use message because it was moved to message_2
-    // println!("{}", message);
-    println!("{}", message_2);
+   let message = String::from("Hello");
+   print_message(message);
 }
 
-
-fn add(x: u32, y: u32) -> u32 {
-    let sum = x + y;
-    sum
+fn print_message(a: String) {
+    println!("{}", a);
+    let c = a;
 }
+
