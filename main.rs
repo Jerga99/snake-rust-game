acdb1c96d996233010b3cde80d3bebea61d007e6
Filip Jerga <jerga99@gmail.com>
2021-11-30 12:50:43 +0100

    String heap

--- main.rs
+++ main.rs
@@ -1,10 +1,14 @@
 
 
 fn main() {
+    // you can place on stack only values with static size
     let a = 10;
     let b = a;
     let c = 15;
     let d = add(a, b);
+
+    let message = String::from("Hello");
+    println!("{}", message);
 }
 
 
