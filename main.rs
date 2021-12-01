8945e22fa892d93911f254136aa0bd25dd61d430
Filip Jerga <jerga99@gmail.com>
2021-12-01 14:26:20 +0100

    Mutable borrow example

--- main.rs
+++ main.rs
@@ -2,17 +2,16 @@
 
 fn main() {
     let mut message = String::from("Hello");
-    let message_2: &mut String = &mut message;
-    // message_2 is not owner of data
-    // message_2 is "borrowing" a reference to message
+    let message_3 = &message;
+    println!("{}", message_3);
+    let message_2 = &mut message;
 
-    message_2.push_str(" World");
-
-    println!("{}", message_2);
+    unpredictable_mutate(message_2);
     println!("{}", message);
 }
-// message and message_2 going out of the scope
-// message_2 is not dropped because it doesn't have ownership of what it refers to
-// message is dropped
+
+fn unpredictable_mutate(val: &mut String) {
+    val.push_str("_unpredictable");
+}
 
 
