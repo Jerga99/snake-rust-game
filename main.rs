4fa671784bc731e96d6bcb3847b37f982ca3d88a
Filip Jerga <jerga99@gmail.com>
2021-12-01 11:55:08 +0100

    Mutable borrow

--- main.rs
+++ main.rs
@@ -1,13 +1,15 @@
 
 
 fn main() {
-    let message = String::from("Hello");
-    let message_2: &String = &message;
+    let mut message = String::from("Hello");
+    let message_2: &mut String = &mut message;
     // message_2 is not owner of data
     // message_2 is "borrowing" a reference to message
 
-    println!("{}", message);
+    message_2.push_str(" World");
+
     println!("{}", message_2);
+    println!("{}", message);
 }
 // message and message_2 going out of the scope
 // message_2 is not dropped because it doesn't have ownership of what it refers to
