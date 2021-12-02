50f1d7cbe0fe003348f50d33b26e4e9961becd3d
Filip Jerga <jerga99@gmail.com>
2021-12-02 14:36:31 +0100

    String vs &str

--- main.rs
+++ main.rs
@@ -1,26 +1,9 @@
 
 
 fn main() {
-    let a = 10;
-    let b = &a;
-    let mut c = &b;
-    let d = b;
+    let mut message = String::from("Hello");
+    let name = "Filip";
 
-    let e = &&100;
-    c = e;
-
-
-    println!("Value of c: {:p}", c);
-    println!("Value of e: {:p}", e);
-    println!("Address of 100: {:p}", &(**e));
-
-    println!("Value of c: {:p}", *c);
-    println!("Value of e: {:p}", *e);
-    println!("Address of 100: {:p}", &(**c));
-
-
-    // println!("Address of a: {:p}", &a);
-    // println!("Value of b: {:p}", b);
-    // println!("Value of c: {:p}", c);
-    // println!("Value of d: {:p}", d);
+    message.push_str(" World");
+    // name.push_str(" Jerga");
 }
