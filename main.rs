7ff91260fda2eddf47e93410394a726e1f8e366a
Filip Jerga <jerga99@gmail.com>
2021-12-02 15:20:38 +0100

    Struct

--- main.rs
+++ main.rs
@@ -1,8 +1,18 @@
 
 
-fn main() {
-    let num = 32;
-    let num_3 = Box::new(100);
 
-    println!("{}", num_3);
+struct Person {
+    name: String,
+    last_name: String,
+    age: u32,
+}
+
+fn main() {
+   let person =  Person {
+       name: "Filip".to_string(),
+       last_name: "Jerga".to_string(),
+       age: 30,
+   };
+
+   println!("{} {} {}", person.name, person.last_name, person.age);
 }
