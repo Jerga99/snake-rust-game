e3cc8f21b70662265f72a60da2a91443d0cd6807
Filip Jerga <jerga99@gmail.com>
2021-12-02 15:01:25 +0100

    String slice +

--- main.rs
+++ main.rs
@@ -3,8 +3,10 @@
 fn main() {
     let mut message = String::from("Hello");
     let slice = &message[2..4]; // 2 -> 3
-    // H E L L O
-    // 0 1 2 3 4
+
+    // message.clear();
 
     println!("{}", slice);
 }
+
+fn move_me(val: String) {}
