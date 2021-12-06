d5151e7612f2e0efc8499773279423d7656b1801
Filip Jerga <jerga99@gmail.com>
2021-12-06 10:57:20 +0100

    import options

--- main.rs
+++ main.rs
@@ -1,9 +1,11 @@
 
-use snake_game::Person;
-use snake_game::Animal;
-use snake_game::log_info;
-use snake_game::log_info_2;
+// use snake_game::Person;
+// use snake_game::Animal;
+// use snake_game::log_info;
+// use snake_game::log_info_2;
 
+// use snake_game::*;
+use snake_game::{Person, Animal, log_info, log_info_2};
 
 fn main() {
     let mut person = Person::new();
--- src/lib.rs
+++ src/lib.rs
@@ -9,69 +9,69 @@ pub trait Log {
 
 #[derive(Debug)]
 enum PersonId {
-Passport(u32),
-IndentityCard(u32, u32, u32),
+    Passport(u32),
+    IndentityCard(u32, u32, u32),
 }
 
 pub struct Person {
-  name: String, // fields
-  last_name: String,
-  age: u32,
-  id: PersonId,
+    name: String, // fields
+    last_name: String,
+    age: u32,
+    id: PersonId,
 }
 
 pub struct Animal(pub String);
 
 impl Log for Animal {
-  fn display_info(&self) {
-      println!("{}", self.0)
-  }
+    fn display_info(&self) {
+        println!("{}", self.0)
+    }
 
-  fn alert_something(&self) {
-      println!("ANIMAL implementation!!!!!!!")
-  }
+    fn alert_something(&self) {
+        println!("ANIMAL implementation!!!!!!!")
+    }
 }
 
 impl Log for Person {
-  fn display_info(&self) {
-      println!("{} {} {} {:?}", self.name, self.last_name, self.age, self.id)
-  }
+    fn display_info(&self) {
+        println!("{} {} {} {:?}", self.name, self.last_name, self.age, self.id)
+    }
 }
 
 impl Person {
-  pub fn new() -> Person {
-      Person {
-          name: "Default".to_string(),
-          last_name: "Default".to_string(),
-          age: 0,
-          id: PersonId::IndentityCard(540, 320, 100)
-      }
-  }
+    pub fn new() -> Person {
+        Person {
+            name: "Default".to_string(),
+            last_name: "Default".to_string(),
+            age: 0,
+            id: PersonId::IndentityCard(540, 320, 100)
+        }
+    }
 
-  fn from(name: String, last_name: String, age: u32, id: PersonId) -> Person {
-      Person {
-          name,
-          last_name,
-          age,
-          id
-      }
-  }
+    fn from(name: String, last_name: String, age: u32, id: PersonId) -> Person {
+        Person {
+            name,
+            last_name,
+            age,
+            id
+        }
+    }
 
-  pub fn change_age(&mut self, new_age: u32) {
-     self.age = new_age;
-  }
+    pub fn change_age(&mut self, new_age: u32) {
+        self.age = new_age;
+    }
 }
 
 // impl makes the compiler determine type at the compile time
 // it will create multiple versions of the function, depending on
 // how many types Log trait implements (Person, Animal)
 pub fn log_info(val: impl Log) {
-  val.alert_something();
+    val.alert_something();
 }
 
 
 // dyn is short for dynamic, and says that function should perform dynamic dispatch
 // decission of exactly which function to call at the runtime
 pub fn log_info_2(val: &dyn Log) {
-  val.alert_something();
+    val.alert_something();
 }
