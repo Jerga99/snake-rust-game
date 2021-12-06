39c2a94bf3b5e03ec5bde6e5b9a59255e96177ce
Filip Jerga <jerga99@gmail.com>
2021-12-06 12:13:06 +0100

    Super keyword

--- main.rs
+++ main.rs
@@ -1,13 +1,9 @@
 
 
-use snake_game::learnig_rust::{Person, PersonId};
+use snake_game::education::learnig_rust::{Person, Log};
 // use snake_game::learnig_rust::top_level;
 
 fn main() {
     let person = Person::new();
-    // person.display_info();
-
-    let id = PersonId::Passport(432);
-    println!("{:?}", id);
-    println!("{}", person.name());
+    person.display_info();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,107 +1,108 @@
 
 
-pub mod learnig_rust {
+fn outsider() {
+    println!("outsider fn!");
+}
 
-    mod top_level {
-        pub fn hi_there() {
-            println!("hi there!");
-        }
+pub mod education {
+    pub mod learnig_rust {
 
-        pub mod low_level {
-            pub fn hello_world() {
-                println!("hello world!");
+        mod top_level {
+            pub fn hi_there() {
+                println!("hi there!");
             }
-        }
-    }
 
-
-    pub trait Log {
-        fn display_info(&self);
-        fn alert_something(&self) {
-            println!("Default implementation!!!!!!!")
-        }
-    }
-
-    #[derive(Debug)]
-    pub enum PersonId {
-        Passport(u32),
-        IndentityCard(u32, u32, u32),
-    }
-
-    pub struct Person {
-        name: String, // fields
-        last_name: String,
-        age: u32,
-        id: PersonId,
-    }
-
-    pub struct Animal(pub String);
-
-    impl Log for Animal {
-        fn display_info(&self) {
-            println!("{}", self.0)
-        }
-
-        fn alert_something(&self) {
-            println!("ANIMAL implementation!!!!!!!")
-        }
-    }
-
-    impl Log for Person {
-        fn display_info(&self) {
-
-            // absolute path
-            // crate points to -> src/lib.rs and src/main.rs
-            crate::learnig_rust::top_level::hi_there();
-            crate::learnig_rust::top_level::low_level::hello_world();
-
-            // relative path
-            top_level::hi_there();
-            top_level::low_level::hello_world();
-
-            println!("{} {} {} {:?}", self.name, self.last_name, self.age, self.id)
-        }
-    }
-
-    impl Person {
-        pub fn new() -> Person {
-            Person {
-                name: "Default".to_string(),
-                last_name: "Default".to_string(),
-                age: 0,
-                id: PersonId::IndentityCard(540, 320, 100)
+            pub mod low_level {
+                pub fn hello_world() {
+                    println!("hello world!");
+                }
             }
         }
 
-        pub fn from(name: String, last_name: String, age: u32, id: PersonId) -> Person {
-            Person {
-                name,
-                last_name,
-                age,
-                id
+
+        pub trait Log {
+            fn display_info(&self);
+            fn alert_something(&self) {
+                println!("Default implementation!!!!!!!")
             }
         }
 
-        pub fn name(&self) -> &String {
-            &self.name
+        #[derive(Debug)]
+        pub enum PersonId {
+            Passport(u32),
+            IndentityCard(u32, u32, u32),
         }
 
-        pub fn change_age(&mut self, new_age: u32) {
-            self.age = new_age;
+        pub struct Person {
+            name: String, // fields
+            last_name: String,
+            age: u32,
+            id: PersonId,
         }
-    }
 
-    // impl makes the compiler determine type at the compile time
-    // it will create multiple versions of the function, depending on
-    // how many types Log trait implements (Person, Animal)
-    pub fn log_info(val: impl Log) {
-        val.alert_something();
-    }
+        pub struct Animal(pub String);
+
+        impl Log for Animal {
+            fn display_info(&self) {
+                println!("{}", self.0)
+            }
+
+            fn alert_something(&self) {
+                println!("ANIMAL implementation!!!!!!!")
+            }
+        }
+
+        impl Log for Person {
+            fn display_info(&self) {
+
+                crate::outsider();
+                // super -> going outside of current module, ..
+                super::super::outsider();
+
+                println!("{} {} {} {:?}", self.name, self.last_name, self.age, self.id)
+            }
+        }
+
+        impl Person {
+            pub fn new() -> Person {
+                Person {
+                    name: "Default".to_string(),
+                    last_name: "Default".to_string(),
+                    age: 0,
+                    id: PersonId::IndentityCard(540, 320, 100)
+                }
+            }
+
+            pub fn from(name: String, last_name: String, age: u32, id: PersonId) -> Person {
+                Person {
+                    name,
+                    last_name,
+                    age,
+                    id
+                }
+            }
+
+            pub fn name(&self) -> &String {
+                &self.name
+            }
+
+            pub fn change_age(&mut self, new_age: u32) {
+                self.age = new_age;
+            }
+        }
+
+        // impl makes the compiler determine type at the compile time
+        // it will create multiple versions of the function, depending on
+        // how many types Log trait implements (Person, Animal)
+        pub fn log_info(val: impl Log) {
+            val.alert_something();
+        }
 
 
-    // dyn is short for dynamic, and says that function should perform dynamic dispatch
-    // decission of exactly which function to call at the runtime
-    pub fn log_info_2(val: &dyn Log) {
-        val.alert_something();
+        // dyn is short for dynamic, and says that function should perform dynamic dispatch
+        // decission of exactly which function to call at the runtime
+        pub fn log_info_2(val: &dyn Log) {
+            val.alert_something();
+        }
     }
 }
