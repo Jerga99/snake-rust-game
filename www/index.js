5a95e95d29487b1909eec22f59964fec04ba8215
Filip Jerga <jerga99@gmail.com>
2021-12-07 12:56:06 +0100

    import to wasm

--- www/index.js
+++ www/index.js
@@ -3,11 +3,23 @@
 
 async function init() {
 
+  const importObject = {
+    console: {
+      log: () => {
+        console.log("Just logging something!");
+      },
+      error: () => {
+        console.log("I am just error");
+      }
+    }
+  }
+
   const response = await fetch("sum.wasm");
   const buffer = await response.arrayBuffer();
-  const wasm = await WebAssembly.instantiate(buffer);
+  const wasm = await WebAssembly.instantiate(buffer, importObject);
 
   const sumFunction = wasm.instance.exports.sum;
+  debugger
   const result = sumFunction(100, 300);
   console.log(result);
 }
