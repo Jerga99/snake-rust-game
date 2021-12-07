1e2a4999ad6f626e29d3df2c453e68443ce6768b
Filip Jerga <jerga99@gmail.com>
2021-12-07 11:58:45 +0100

    Dev Server

---
+++ www/webpack.config.js
@@ -0,0 +1,11 @@
+
+const path = require("path");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "index.js"
+  },
+  mode: "development"
+}
