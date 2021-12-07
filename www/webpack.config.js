5cb3f3b93c02e4783c4b22e9a83d772c0d314e0e
Filip Jerga <jerga99@gmail.com>
2021-12-07 12:39:35 +0100

    Copy plugin

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,5 +1,6 @@
 
 const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./index.js",
@@ -7,5 +8,12 @@ module.exports = {
     path: path.resolve(__dirname, "public"),
     filename: "index.js"
   },
-  mode: "development"
+  mode: "development",
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./index.html", to: "./"}
+      ]
+    })
+  ]
 }
