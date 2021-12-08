856674a2e664e8c1d0507443e59c6d3a33283bda
Filip Jerga <jerga99@gmail.com>
2021-12-08 13:08:26 +0100

    Import function to webassembly

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,12 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
-    println!("Hi there {}", name);
+   alert(name);
+}
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
 }
 
 // wasm-pack build --target web
--- www/index.js
+++ www/index.js
@@ -3,5 +3,4 @@ import init, { greet } from "snake_game";
 
 init().then(_ => {
   greet("Filip");
-  console.log("OK!");
 })
