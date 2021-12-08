82a4506edbbc6dd187d03ef8ae59d742ed570b72
Filip Jerga <jerga99@gmail.com>
2021-12-08 12:40:18 +0100

    Init Our Code

--- www/index.js
+++ www/index.js
@@ -1,30 +1,7 @@
 
+import init, { greet } from "snake_game";
 
-
-async function init() {
-  const memory = new WebAssembly.Memory({initial: 1})
-  const importObject = {
-    js: {
-      mem: memory
-    },
-    console: {
-      log: () => {
-        console.log("Just logging something!");
-      },
-      error: () => {
-        console.log("I am just error");
-      }
-    }
-  }
-
-  const response = await fetch("sum.wasm");
-  const buffer = await response.arrayBuffer();
-
-  const wasm = await WebAssembly.instantiate(buffer, importObject);
-  const uint8Array = new Uint8Array(memory.buffer, 0, 2);
-
-  const hiText = new TextDecoder().decode(uint8Array);
-  console.log(hiText);
-}
-
-init();
+init().then(_ => {
+  greet("Filip");
+  console.log("OK!");
+})
