bee1f5a9b5cccf4601b0841455e0731f80c72147
Filip Jerga <jerga99@gmail.com>
2021-12-09 08:47:22 +0100

    bootstrap

---
+++ www/bootstrap.js
@@ -0,0 +1,6 @@
+
+
+
+import("./index.js")
+  .catch(e => console.error("Error importing index.js :", e))
+
--- www/webpack.config.js
+++ www/webpack.config.js
@@ -3,10 +3,10 @@ const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: "./index.js",
+  entry: "./bootstrap.js",
   output: {
     path: path.resolve(__dirname, "public"),
-    filename: "index.js"
+    filename: "bootstrap.js"
   },
   mode: "development",
   plugins: [
