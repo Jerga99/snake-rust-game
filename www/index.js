dba6017aad9f601d30e71e8627593f7d47e3d36a
Filip Jerga <jerga99@gmail.com>
2021-12-09 10:21:32 +0100

    Get canvas

--- www/index.js
+++ www/index.js
@@ -2,7 +2,7 @@
 import init, { World } from "snake_game";
 
 init().then(_ => {
-
   const world = World.new();
-  console.log(world.width());
+  const canvas = document.getElementById("snake-canvas");
+  const ctx = canvas.getContext("2d");
 })
