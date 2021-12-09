1c81449200563da41d4cb0db472dd7b0122e770e
Filip Jerga <jerga99@gmail.com>
2021-12-09 10:43:25 +0100

    Draw World

--- www/index.js
+++ www/index.js
@@ -2,7 +2,32 @@
 import init, { World } from "snake_game";
 
 init().then(_ => {
+  const CELL_SIZE = 20;
+
   const world = World.new();
+  const worldWidth = world.width();
+
   const canvas = document.getElementById("snake-canvas");
   const ctx = canvas.getContext("2d");
+
+  canvas.height = worldWidth * CELL_SIZE;
+  canvas.width = worldWidth * CELL_SIZE;
+
+  function drawWorld() {
+    ctx.beginPath();
+
+    for (let x = 0; x < worldWidth + 1; x++) {
+      ctx.moveTo(CELL_SIZE * x, 0);
+      ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE)
+    }
+
+    for (let y = 0; y < worldWidth + 1; y++) {
+      ctx.moveTo(0, CELL_SIZE * y);
+      ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y)
+    }
+
+    ctx.stroke();
+  }
+
+  drawWorld();
 })
