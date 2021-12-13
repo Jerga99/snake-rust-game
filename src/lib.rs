d694f87b61eaf60158af6bd1826d11e2a4062292
Filip Jerga <jerga99@gmail.com>
2021-12-13 13:46:12 +0100

    Check if body contains reward

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub enum Direction {
     Left
 }
 
-#[derive(Clone, Copy)]
+#[derive(PartialEq, Clone, Copy)]
 pub struct SnakeCell(usize);
 
 struct Snake {
@@ -56,13 +56,19 @@ pub struct World {
 impl World {
     pub fn new(width: usize, snake_idx: usize) -> World {
 
+        let snake = Snake::new(snake_idx, 3);
         let size = width * width;
-        let reward_cell = rnd(size);
+        let mut reward_cell;
+
+        loop {
+          reward_cell = rnd(size);
+          if !snake.body.contains(&SnakeCell(reward_cell)) { break; }
+        }
 
         World {
             width,
             size,
-            snake: Snake::new(snake_idx, 3),
+            snake,
             next_cell: None,
             reward_cell,
         }
