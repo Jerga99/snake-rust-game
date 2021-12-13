05a5fc9c6f2943058c8d4987c51022072e01cca8
Filip Jerga <jerga99@gmail.com>
2021-12-13 11:45:46 +0100

    Move cells

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ pub enum Direction {
     Left
 }
 
+#[derive(Clone)]
 pub struct SnakeCell(usize);
 
 struct Snake {
@@ -74,8 +75,15 @@ impl World {
     }
 
     pub fn step(&mut self) {
+        let temp = self.snake.body.clone();
         let next_cell = self.gen_next_snake_cell();
         self.snake.body[0] = next_cell;
+
+        let len = self.snake.body.len();
+
+        for i in 1..len {
+            self.snake.body[i] = SnakeCell(temp[i - 1].0);
+        }
     }
 
     fn gen_next_snake_cell(&self) -> SnakeCell {
