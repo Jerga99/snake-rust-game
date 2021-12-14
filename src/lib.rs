3dad42c28a9de30276ccda76d0cd17b08b3f4741
Filip Jerga <jerga99@gmail.com>
2021-12-14 09:48:26 +0100

    Fn to gen reward

--- src/lib.rs
+++ src/lib.rs
@@ -58,20 +58,25 @@ impl World {
 
         let snake = Snake::new(snake_idx, 3);
         let size = width * width;
-        let mut reward_cell;
-
-        loop {
-          reward_cell = rnd(size);
-          if !snake.body.contains(&SnakeCell(reward_cell)) { break; }
-        }
 
         World {
             width,
             size,
+            reward_cell: World::gen_reward_cell(size, &snake.body),
             snake,
             next_cell: None,
-            reward_cell,
         }
+    }
+
+    fn gen_reward_cell(max: usize, snake_body: &Vec<SnakeCell>) -> usize {
+        let mut reward_cell;
+
+        loop {
+          reward_cell = rnd(max);
+          if !snake_body.contains(&SnakeCell(reward_cell)) { break; }
+        }
+
+        reward_cell
     }
 
     pub fn width(&self) -> usize {
@@ -124,6 +129,7 @@ impl World {
 
         if self.reward_cell == self.snake_head_idx() {
             self.snake.body.push(SnakeCell(self.snake.body[1].0));
+            self.reward_cell = World::gen_reward_cell(self.size, &self.snake.body);
         }
     }
 
